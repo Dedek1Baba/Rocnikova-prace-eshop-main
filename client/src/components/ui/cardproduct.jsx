b3f20bf4cada06e3129df4f33f3a49ad40a2b9c4
Dedek1Baba <jozef.tomko@gmail.com>
2025-02-13 17:30:36 +0100

    upraveni cardproductu

--- client/src/components/ui/cardproduct.jsx
+++ client/src/components/ui/cardproduct.jsx
@@ -2,6 +2,7 @@ import { Card, CardContent } from "./card";
 import { Button } from "./button";
 import bhoodie from "../../assets/bhoodie.png";
 import phoodie from "../../assets/phoodie.png";
+import { ArrowRight } from 'lucide-react';
 
 const products = [
   {
@@ -26,55 +27,40 @@ const products = [
 
 export default function cardproduct() {
   return (
-    <section className="py-16 bg-black text-white">
-      <div className="container mx-auto text-center">
-        <Card className="bg-black border-4 border-white rounded-3xl p-6 px-2 shadow-2xl">
-          <h2 className="text-4xl font-semibold mb-8 uppercase tracking-wide">
+      <div className="bg-black p-8 text-center text-white">
+          <h2 className="text-4xl font-semibold mb-6 uppercase tracking-wide">
             Naše produkty
-          </h2>
-
-          
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48">
-            {products.map((product) => (
-              <Card className="bg-primary rounded-xl p-3 shadow-lg hover:scale-105 transition-transform duration-300 mx-auto">
-                <CardContent className="flex flex-col items-center">
-
-                  <div className="relative">
+          </h2>  
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center">
+            {products.map((product, index) => (
+              <div key={index} className="text-center">
+              
+                  <div className="relative w-full max-w-fit mx-auto overflow-hidden rounded-2xl">
                     <img
-                      src={product.image}
-                      alt={product.name}
-                      width={200}
-                      height={200}
-                      className="relative z-10 rounded-xl"
+                      src={product.image} 
+                      alt={product.name} 
+                      className="w-full h-auto transition-opacity duration-300 hover:opacity-40"
                       draggable="false"
                       style={{ filter: "drop-shadow(0 0 10px white)" }}
-                    />
-                    <div className="absolute inset-0"></div>
+                    />  
                   </div>
-                  <h3 className="text-lg font-semibold mt-3 text-gray-100">
+                  <h3 className="mt-4 font-bold">
                     {product.name}
                   </h3>
-                  <p className="text-gray-400 mb-2 text-md">{product.price}</p>
-                  <a href={product.link} className="block w-full">
-                    <Button className="bg-gradient-to-br from-primary to-secondary transition-all hover:text-secondary duration-200 text-white font-bold py-2 px-4 rounded-full">
+                  <p className="mt-2">{product.price}</p>
+                  <a href={product.link} className=" ">
+                    <Button className="bg-gradient-to-br from-primary to-secondary transition-all hover:text-secondary duration-200 text-white font-bold mt-2 px-4 py-2 rounded-full">
                       Zobrazit produkt
                     </Button>
-                  </a>
-                </CardContent>
-              </Card>
-            ))}
+                    </a>
           </div>
-
-
-          <div className="mt-8">
-            <a href="#products">
-              <Button className="bg-gradient-to-br from-primary to-secondary hover:text-secondary font-bold py-3 px-6 shadow-lg hover:bg-gray-300 transition-all rounded-full">
-                Více produktů
-              </Button>
-            </a>
-          </div>
-        </Card>
+        ))}
       </div>
-    </section>
+            <a href="#products"className="mt-6 block" >
+              <Button className="bg-gradient-to-br from-primary to-secondary hover:text-secondary font-bold py-3 px-4 shadow-lg hover:bg-gray-300 transition-all rounded-2xl">
+                Více produktů <ArrowRight />
+              </Button>
+              </a>
+         </div>
   );
 }
